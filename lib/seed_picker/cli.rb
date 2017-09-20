79c1aa1cb0fcc7121692056a3cec1d9284966a41
Christine Tran <christinetran825@gmail.com>
2017-09-20 06:28:04 +0000

    redefined call method, added two new methods

--- lib/seed_picker/cli.rb
+++ lib/seed_picker/cli.rb
@@ -1,7 +1,19 @@
 class SeedPicker::CLI
 
   def call
-    puts "Our Seeds"
+    #SeedPicker::Scraper.new.
+    puts "Welcome to Baker Creek Heirloom Seeds RareSeeds."
+    list_seeds
+  end
+
+  def list_seeds
+    puts "Vegetable Seeds"
+    # puts ""
+  end
+
+  def choose_seeds
+    puts "Which vegetable seed would you like to view? A-E, F-J, K-O, P-T, U-Z?"
+    input = gets.strip.to_s.upcase
   end
 
 end

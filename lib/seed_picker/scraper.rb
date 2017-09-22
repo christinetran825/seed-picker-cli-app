f3bbff45e5dfacbc5a3969586ca4494b5379d038
Christine Tran <christinetran825@gmail.com>
2017-09-22 02:48:21 +0000

    defined the get_page method for SeedPicker::Scraper

--- lib/seed_picker/scraper.rb
+++ lib/seed_picker/scraper.rb
@@ -1,9 +1,37 @@
 class SeedPicker::Scraper
 
-  doc = Nokogiri::HTML(open("http://www.rareseeds.com/store/vegetables/"))
-  binding.pry
+  def get_page
+    doc = Nokogiri::HTML(open("http://www.rareseeds.com/store/vegetables/"))
+    binding.pry
+    # doc.css(".sitebody").first.css(".grid_4").first.css(".itemTitle a").attr('href').value #link of Parent seed
+    # doc.css(".sitebody").first.css(".grid_4").first.css(".itemTitle a").text ##Name of Parent Seed
+  end
+
   # scrape_ _index
   #   self.get_page.css
   #
   #   scrape_ _index.each
+
+  def get_seeds
+    self.get_page.css(".sitebody")
+  end
+
+  # def make_seeds
+  #   self.get_seeds.each do |seeds|
+  #
+  #
+  #   end
+  # end
+
+  # def show_seeds
+  #   self.make_seeds
+  #   SeedPicker::Seeds.all.each do |seeds|
+  #
+  #
+  #   end
+  #
+  # end
+
+# SeedPicker::Scraper.new.show_seeds
+
 end

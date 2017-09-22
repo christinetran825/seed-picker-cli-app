0ea06dfd6ef0438339d9e34dfde55d2031d782b2
Christine Tran <christinetran825@gmail.com>
2017-09-22 04:14:07 +0000

    defined methods for SeedPicker::Scraper

--- lib/seed_picker/scraper.rb
+++ lib/seed_picker/scraper.rb
@@ -1,37 +1,35 @@
 class SeedPicker::Scraper
 
+  # binding.pry
+
   def get_page
+    # binding.pry
     doc = Nokogiri::HTML(open("http://www.rareseeds.com/store/vegetables/"))
-    binding.pry
-    # doc.css(".sitebody").first.css(".grid_4").first.css(".itemTitle a").attr('href').value #link of Parent seed
+
+    # doc.css(".sitebody").first.css(".grid_4").first.css(".itemTitle a").attr('href').value #Link of Parent seed
     # doc.css(".sitebody").first.css(".grid_4").first.css(".itemTitle a").text ##Name of Parent Seed
   end
 
-  # scrape_ _index
-  #   self.get_page.css
-  #
-  #   scrape_ _index.each
-
   def get_seeds
-    self.get_page.css(".sitebody")
+    self.get_page.css(".itemTitle a")
   end
 
-  # def make_seeds
-  #   self.get_seeds.each do |seeds|
-  #
-  #
-  #   end
-  # end
+  def make_seeds
+    self.get_seeds.collect do |seeds|
+      grouped_seeds = SeedPicker::Seeds.new
+      grouped_seeds.parent_seed_link = seeds['href'] #Link of Parent seed
+      grouped_seeds.parent_seed_name = seeds.text #Name of Parent seed
+    end
+  end
 
-  # def show_seeds
-  #   self.make_seeds
-  #   SeedPicker::Seeds.all.each do |seeds|
-  #
-  #
-  #   end
-  #
-  # end
-
-# SeedPicker::Scraper.new.show_seeds
+  def print_seeds
+    self.make_seeds
+    SeedPicker::Seeds.all.each do |grouped_seeds|
+      puts "Seed Link: #{grouped_seeds.parent_seed_link}"
+      puts "Seed Name: #{grouped_seeds.parent_seed_name}"
+    end
+  end
 
 end
+
+SeedPicker::Scraper.new.print_seeds

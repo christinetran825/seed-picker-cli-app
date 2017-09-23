755920d86e2c981a60b1b35e4d08f8642e3266dd
Christine Tran <christinetran825@gmail.com>
2017-09-23 02:23:06 +0000

    parsed secondary webpage (URL attr)

--- lib/seed_picker/scraper.rb
+++ lib/seed_picker/scraper.rb
@@ -1,7 +1,9 @@
 class SeedPicker::Scraper
 
   binding.pry
-  #doc = Nokogiri::HTML(open("http://www.rareseeds.com/store/vegetables/"))
+
+  # Main Vegetable Seed page
+  # doc = Nokogiri::HTML(open("http://www.rareseeds.com/store/vegetables/"))
   # doc.css(".sitebody .grid_4").first.css("h3.itemTitle").first.css("a").attr('href').value #Link of Parent seed
   # doc.css(".sitebody .grid_4").first.css("h3.itemTitle").first.css("a").text ##Name of Parent Seed
   # doc.css(".sitebody .grid_4").collect.with_index do |the_seeds, idx|
@@ -9,6 +11,13 @@ class SeedPicker::Scraper
   #   seed_name = the_seeds.css("h3.itemTitle a").text
   #   "(#{idx}). #{seed_name}"
   # end
+
+  # Variety Vegetable Seed Page - URL attr
+  # set = Nokogiri::HTML(open("http://www.rareseeds.com/store/vegetables/amaranth/"))
+  # set.css(".sitebody .grid_9 .mainContent").first.css("div#CT_Main_0_pnlHeading .sectionDesc p").first.text #description
+  # set.css(".sitebody .grid_9 .mainContent .hawksearch").first.css('title').text #parent seed name
+  # set.css(".sitebody .grid_9 .mainContent .hawksearch .grid_4").first.css("h3.itemTitle a").text #variety name
+  # set.css(".sitebody .grid_9 .mainContent .hawksearch .grid_4").first.css(".itemMiniCart .itemPrice").text #variety price
 
   def get_page
     Nokogiri::HTML(open("http://www.rareseeds.com/store/vegetables/"))
@@ -36,7 +45,24 @@ class SeedPicker::Scraper
     end
   end
 
-end
+####
 
+  # def make_links
+  #   # binding.pry
+  #   # variety = Nokogiri::HTML(open("http://www.rareseeds.com/store/vegetables/"))
+  #   self.get_seeds.collect.with_index do |the_seeds, idx|
+  #     # grouped_seeds = SeedPicker::Seeds.new
+  #     link_and_name = {}
+  #     seed_link = the_seeds.css("h3.itemTitle a").attr('href').value
+  #     seed_name = the_seeds.css("h3.itemTitle a").text
+  #     link_and_name["#{seed_name}".to_sym] = "#{seed_link}"
+  #     link_and_name
+  #     # idx += 1
+  #     # "(#{idx}). #{seed_name}"
+  #   end
+  # end
+
+end
+SeedPicker::Scraper.new.make_links
 
 SeedPicker::Scraper.new.print_seeds

08def798f5ee20c943988493fe894838d0b1fa01
Christine Tran <christinetran825@gmail.com>
2017-09-26 09:32:02 +0000

    updated initialize method

--- lib/seed_picker/seeds.rb
+++ lib/seed_picker/seeds.rb
@@ -2,8 +2,7 @@ class SeedPicker::Seeds
 
   # binding.pry
 
-  attr_accessor :letter, :parent_seed_name, :parent_seed_url
-  # :parent_seed_description, :variety_name, :variety_name_description, :price, :variety_seed_url
+  attr_accessor :letter, :parent_seed_name, :parent_seed_url, :parent_seed_description, :variety_name, :price,
 
   @@all = []
 
@@ -11,6 +10,9 @@ class SeedPicker::Seeds
     @letter = letter
     @parent_seed_name = parent_seed_name
     @parent_seed_url = parent_seed_url
+    @parent_seed_description = parent_seed_description
+    @variety_name = variety_name
+    @price = price
     # @@all << self
   end
 

f3c2e227a40d516f97591f07e0c09c409100964f
Christine Tran <christinetran825@gmail.com>
2017-09-26 02:57:23 +0000

    updated methods

--- lib/seed_picker/scraper.rb
+++ lib/seed_picker/scraper.rb
@@ -1,5 +1,5 @@
 class SeedPicker::Scraper
-# binding.pry
+binding.pry
   def get_page
     Nokogiri::HTML(open("http://www.rareseeds.com/store/vegetables/"))
   end
@@ -9,7 +9,6 @@ class SeedPicker::Scraper
   end
 
   def make_seeds
-    # binding.pry
     self.get_seeds.collect do |the_seeds|
       seed_name = the_seeds.inner_text
       seed_name
@@ -24,8 +23,34 @@ class SeedPicker::Scraper
       "#{key} = #{value}"
     end
   end
-  
+
+  # binding.pry
+  def list_parents
+    grouped_seeds = self.make_seeds.group_by do |seed|
+      seed[0]
+    end
+    hash = {}
+    grouped_seeds.each do |key, value|
+      hash[key] = value
+    end
+    hash
+  end
+
+  def choose_parent_seed
+    input = gets.strip.to_s.upcase
+    parent_seeds = self.list_parents[input]
+    parent_seeds.collect.with_index(1) do |key, index|
+      "#{index}. #{key}"
+    end
+  end
+
+ SeedPicker::Scraper.new.choose_parent_seed
+
+
+
+
 end
+
 
 # SeedPicker::Scraper.new.print_seeds => lists all the vegetable seeds in grouped letters as a hash. parent seeds are grouped in an array
 # SeedPicker::Scraper.new.print_seeds["A"] = ["Amaranth", "Artichoke & Cardoon", "Asparagus"]

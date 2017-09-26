e2c02344a65d51d2f0563bc758919900d8f55ee4
Christine Tran <christinetran825@gmail.com>
2017-09-26 08:18:39 +0000

    redefined initialize method

--- lib/seed_picker/seeds.rb
+++ lib/seed_picker/seeds.rb
@@ -8,24 +8,15 @@ class SeedPicker::Seeds
   @@all = []
 
   def initialize
-    @@all << self
+    @letter = letter
+    @parent_seed_name = parent_seed_name
+    @parent_seed_url = parent_seed_url
+    # @@all << self
   end
 
   def self.all
     @@all
   end
 
-  # def self.scrape_variety_seeds
-  #   set = Nokogiri::HTML(open(url))
-  #
-  #   seed = self.new
-  #   seed.parent_seed_description = set.css(".sitebody .grid_9 .mainContent").first.css("div#CT_Main_0_pnlHeading .sectionDesc p").first.text #description
-  #   # set.css(".sitebody .grid_9 .mainContent .hawksearch").first.css('title').text #parent seed name
-  #   seed.variety_name = set.css(".sitebody .grid_9 .mainContent .hawksearch .grid_4").first.css("h3.itemTitle a").text #variety name
-  #   seed.price = set.css(".sitebody .grid_9 .mainContent .hawksearch .grid_4").first.css(".itemMiniCart .itemPrice").text #variety price
-  #
-  #   seed
-  #
-  # end
 
 end

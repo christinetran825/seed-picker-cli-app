f4ccb87d372e4019c3fb589378cba6ea43375aa4
Christine Tran <christinetran825@gmail.com>
2017-09-26 19:49:54 +0000

    redefined attr accessors

--- lib/seed_picker/seeds.rb
+++ lib/seed_picker/seeds.rb
@@ -2,28 +2,45 @@ class SeedPicker::Seeds
 
   # binding.pry
 
-  attr_accessor :letter, :parent_seed_name, :parent_seed_url, :url_id, :parent_seed_description, :variety_name, :price
+  attr_accessor :parent_letter, :parent_seed_name, :parent_seed_url, :parent_seed_url_id, :veggie_description, :variety_name, :price
 
   @@all = []
 
-  def initialize
-    @letter = letter
+  def initialize #new instances have the below methods/properties
+    @parent_letter = parent_letter
     @parent_seed_name = parent_seed_name
     @parent_seed_url = parent_seed_url
-    @parent_seed_description = parent_seed_description
+    @parent_seed_url_id = parent_seed_url_id
+    @veggie_description = veggie_description
     @variety_name = variety_name
     @price = price
-    @@all << self
+    @@all << self #all instances get added into @@all array
   end
 
   def self.all
     @@all
   end
 
-  def self.parent_seed
-    self.all.collect do |seed_name|
-      puts seed_name.parent_seed_name
+  def self.veggie_seed #listing all vegetable seeds grouped by first letter
+    # binding.pry
+    self.all.collect do |veggie|
+      puts "#{veggie.parent_letter} : #{veggie.parent_seed_name}"
     end
   end
+
+  def self.parent_seed #listing all parent_seeds by index upon user input
+    # binding.pry
+    self.all.collect.with_index(1) do |veggie, index|
+      puts "#{index}. #{veggie.parent_seed_name}"
+    end
+  end
+
+  def self.parent_description
+    self.all.collect do |parents|
+      parents.veggie_description
+    end
+  end
+
+
 
 end

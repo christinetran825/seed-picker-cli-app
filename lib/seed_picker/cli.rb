6155648b18242db5eb93aee2a0dba1180c89fe08
Christine Tran <christinetran825@gmail.com>
2017-09-26 19:49:22 +0000

    redefined Seeds method in CLI methods

--- lib/seed_picker/cli.rb
+++ lib/seed_picker/cli.rb
@@ -1,19 +1,17 @@
 class SeedPicker::CLI
 
   def call
+    SeedPicker::Scraper.scrape_parent_seeds #CLI calls on the scraper class to tell Nokogiri to scrape the website
     puts "* * * Welcome to Baker Creek Heirloom Seeds RareSeeds * * * "
     choose_a_letter
   end
 
   def choose_a_letter
-
     puts ""
     puts "^ - ^ Please choose a group of seeds by its letter from A-Z. Type exit to exit." # user is prompted with a demand
     puts ""
     puts "  ------------- Vegetable Seeds -------------"   # user sees a list of vegetable seeds
-    SeedPicker::Scraper.scrape_parent_seeds #CLI calls on the scraper class to tell Nokogiri to scrape the website
-    SeedPicker::Seeds.parent_seed
-    # puts SeedPicker::Scraper.new.print_seeds ## scrape the seeds into groups starting with the seeds' name's first letter
+    SeedPicker::Seeds.veggie_seed ## Seeds class veggie_seed method
     puts ""
 
     # user inputs the letter of the parent seed, that letter gives us a list of all the seeds within that letter starting with an index of 1.
@@ -34,10 +32,11 @@ class SeedPicker::CLI
     puts ""
     puts "^ - ^ Please choose a seed by its number. Type back to go back to the list of seeds. Type exit to exit." # user is prompted with a choice
     puts ""
-    # puts "     ----- Group A -----" # user sees a list of all parent seeds with an index of 1.
+    puts "     ----- Group  -----" # user sees a list of all parent seeds with an index of 1.
     # puts "     1. Amaranth"
     # puts "     2. Artichoke & Cardoon"
     # puts "     3. Asparagus"
+    SeedPicker::Seeds.parent_seed
     puts ""
 
     # user inputs the number corresponding to the parent seed they want to view

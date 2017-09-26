941004099cde7dabc39280b144fc79894931f1cc
Christine Tran <christinetran825@gmail.com>
2017-09-26 20:41:27 +0000

    updated attr accessors

--- lib/seed_picker/seeds.rb
+++ lib/seed_picker/seeds.rb
@@ -1,7 +1,5 @@
 class SeedPicker::Seeds
 
-  # binding.pry
-
   attr_accessor :parent_letter, :parent_seed_name, :parent_seed_url, :parent_seed_url_id, :veggie_description, :variety_name, :price
 
   @@all = []
@@ -22,22 +20,20 @@ class SeedPicker::Seeds
   end
 
   def self.veggie_seed #listing all vegetable seeds grouped by first letter
-    # binding.pry
     self.all.collect do |veggie|
       puts "#{veggie.parent_letter} : #{veggie.parent_seed_name}"
     end
   end
 
   def self.parent_seed #listing all parent_seeds by index upon user input
-    # binding.pry
     self.all.collect.with_index(1) do |veggie, index|
       puts "#{index}. #{veggie.parent_seed_name}"
     end
   end
 
   def self.parent_description
-    self.all.collect do |parents|
-      parents.veggie_description
+    self.all.collect do |veggie|
+      veggie.parent_seed_url
     end
   end
 

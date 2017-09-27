d225a0d7e4b817688803015fe1c5803ab3636fe4
Christine Tran <christinetran825@gmail.com>
2017-09-27 22:02:57 +0000

    removed variety scraper method that were commented

--- lib/seed_picker/scraper.rb
+++ lib/seed_picker/scraper.rb
@@ -2,8 +2,6 @@ class SeedPicker::Scraper
 
 # binding.pry
 
-  attr_accessor :parent_letter, :parent_seed_name, :parent_seed_url, :veggie_description, :variety_name, :price
-
   def self.scrape_parent_seeds #scraping the parent seed for the url, url_id, parent_seed_name, and letter
     doc = Nokogiri::HTML(open("http://www.rareseeds.com/store/vegetables/"))
     doc.css(".sitebody .grid_4 h3.itemTitle a").collect do |the_seeds|
@@ -14,17 +12,5 @@ class SeedPicker::Scraper
       seed
     end
   end
-
-  # def self.scrape_variety_seeds
-  #   set = Nokogiri::HTML(open(parent_seed_url))
-  #   set.css(".sitebody .grid_9 .mainContent").collect do |the_types|
-  #     varieties = SeedPicker::Seeds.new
-  #     varieties.veggie_description = the_types.css("div#CT_Main_0_pnlHeading .sectionDesc p").first.inner_text.gsub(/\r\n\t/, "") #description
-  #     # set.css(".sitebody .grid_9 .mainContent .hawksearch").first.css('title').text #parent seed name
-  #     varieties.variety_name = the_types.css(".hawksearch .grid_4 h3.itemTitle a").first.inner_text #variety name
-  #     varieties.price = the_types.css(".hawksearch .grid_4 .itemMiniCart .itemPrice").first.inner_text #variety price
-  #     varieties
-  #   end
-  # end
 
 end

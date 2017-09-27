582fee1c231316cecae20f85c3dd7999cf5a1976
Christine Tran <christinetran825@gmail.com>
2017-09-27 16:26:00 +0000

    testing method for parent_seed_url

--- lib/seed_picker/seeds.rb
+++ lib/seed_picker/seeds.rb
@@ -31,25 +31,41 @@ class SeedPicker::Seeds
     end
   end
 
-
-## Scraping the variety details
-  #
-  def self.scrape_variety_seeds
-    variety = self.all.collect do |veggie| veggie.parent_seed_url end
-    Nokogiri::HTML(open(self.parent_seed_url))
-
+  def self.scrape_parent_seeds #scraping the parent seed for the url, url_id, parent_seed_name, and letter
+    doc = Nokogiri::HTML(open("http://www.rareseeds.com/store/vegetables/"))
+    doc.css(".sitebody .grid_4 h3.itemTitle a").collect do |the_seeds|
+      seed = SeedPicker::Seeds.new ##!!!!! CALLING a new instance OBJECT !!!!!!!!
+      seed.parent_seed_url = the_seeds["href"]
+      seed.parent_seed_name = the_seeds.inner_text
+      seed.parent_letter = the_seeds.inner_text[0]
+            finder = Nokogiri::HTML(open(seed.parent_seed_url))
+      seed.veggie_description = finder.css(".sitebody .grid_9 .mainContent div#CT_Main_0_pnlHeading .sectionDesc p").first.inner_text.gsub(/\r\n\t/, "") #description
+      seed.variety_name = finder.css(".sitebody .grid_9 .mainContent .hawksearch .grid_4 h3.itemTitle a").first.inner_text #variety name
+      seed.price = finder.css(".sitebody .grid_9 .mainContent .hawksearch .grid_4 .itemMiniCart .itemPrice").first.inner_text  #variety price
+      seed
+    end
   end
 
-  def veggie_description
-    @veggie_description ||= .css(".sitebody .grid_9 .mainContent div#CT_Main_0_pnlHeading .sectionDesc p").first.inner_text.gsub(/\r\n\t/, "") #description
-  end
 
-  def variety_name
-    @variety_name ||= .css(".sitebody .grid_9 .mainContent .hawksearch .grid_4 h3.itemTitle a").first.inner_text #variety name
-  end
 
-  def price
-    @price ||= .css(".sitebody .grid_9 .mainContent .hawksearch .grid_4 .itemMiniCart .itemPrice").first.inner_text  #variety price
-  end
-
-end
+# ## Scraping the variety details
+#   #
+#   def self.scrape_variety_seeds
+#     variety = self.all.collect do |veggie| veggie.parent_seed_url end
+#     Nokogiri::HTML(open(self.parent_seed_url))
+#
+#   end
+#
+#   def veggie_description
+#     @veggie_description ||= .css(".sitebody .grid_9 .mainContent div#CT_Main_0_pnlHeading .sectionDesc p").first.inner_text.gsub(/\r\n\t/, "") #description
+#   end
+#
+#   def variety_name
+#     @variety_name ||= .css(".sitebody .grid_9 .mainContent .hawksearch .grid_4 h3.itemTitle a").first.inner_text #variety name
+#   end
+#
+#   def price
+#     @price ||= .css(".sitebody .grid_9 .mainContent .hawksearch .grid_4 .itemMiniCart .itemPrice").first.inner_text  #variety price
+#   end
+#
+# end

0e7fb1491b8af66be2ae642ea54463c164c87eec
Christine Tran <christinetran825@gmail.com>
2017-09-27 23:39:31 +0000

    finalized scrape parent seeds method

--- lib/seed_picker/scraper.rb
+++ lib/seed_picker/scraper.rb
@@ -1,5 +1,7 @@
 class SeedPicker::Scraper
 
+binding.pry
+
   def self.scrape_parent_seeds #scraping the parent seed for the url, url_id, parent_seed_name, and letter
     doc = Nokogiri::HTML(open("http://www.rareseeds.com/store/vegetables/"))
     doc.css(".sitebody .grid_4 h3.itemTitle a").collect do |the_seeds|
@@ -7,15 +9,16 @@ class SeedPicker::Scraper
       seed.parent_seed_url = the_seeds["href"]
       seed.parent_seed_name = the_seeds.text
       seed.parent_letter = the_seeds.text[0]
-            finder = Nokogiri::HTML(open(seed.parent_seed_url))
-            finder.css(".sitebody .grid_9 .mainContent").collect do |the_details|
-              seed.veggie_description = finder.css("div#CT_Main_0_pnlHeading .sectionDesc p").text.gsub(/\r\n\t/, "") #description
-              seed.variety_name = finder.css(".hawksearch .grid_4 h3.itemTitle a").text #variety name
-              seed.price = finder.css(".hawksearch .grid_4 .itemMiniCart .itemPrice").text  #variety price
-            end
+        finder = Nokogiri::HTML(open(seed.parent_seed_url))
+        finder.css(".sitebody .grid_9 .mainContent div#CT_Main_0_pnlHeading").collect do |the_details|
+          seed.veggie_description = the_details.css(".sectionDesc p").text.gsub(/\r\n\t/, "") #description
+        end
+        finder.css(".sitebody .grid_9 .mainContent .hawksearch .grid_4").collect do |the_info|
+          seed.variety_name = the_info.css("h3.itemTitle a").text #variety name
+          seed.price = the_info.css(".itemMiniCart .itemPrice").text #variety price
+        end
       seed
     end
   end
-
 
 end

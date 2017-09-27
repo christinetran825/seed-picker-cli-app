7694b96b7d708b9d1ceba5187a6d4fe5d755569a
Christine Tran <christinetran825@gmail.com>
2017-09-27 22:36:15 +0000

    updated self.scrape_parent_seeds with variety scrape

--- lib/seed_picker/scraper.rb
+++ lib/seed_picker/scraper.rb
@@ -1,7 +1,5 @@
 class SeedPicker::Scraper
 
-# binding.pry
-
   def self.scrape_parent_seeds #scraping the parent seed for the url, url_id, parent_seed_name, and letter
     doc = Nokogiri::HTML(open("http://www.rareseeds.com/store/vegetables/"))
     doc.css(".sitebody .grid_4 h3.itemTitle a").collect do |the_seeds|

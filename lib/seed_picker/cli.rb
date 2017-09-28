266eb3ef4436295306fd0f5b8f8f5d463f177f78
Christine Tran <christinetran825@gmail.com>
2017-09-28 23:00:29 +0000

    redefined call method and choose a letter method

--- lib/seed_picker/cli.rb
+++ lib/seed_picker/cli.rb
@@ -3,36 +3,34 @@ class SeedPicker::CLI
   def call
     SeedPicker::Scraper.scrape_parent_seeds #CLI calls on the scraper class to tell Nokogiri to scrape the website
     puts "* * * Welcome to Baker Creek Heirloom Seeds RareSeeds * * * "
+    puts ""
+    puts "Here's our collection of vegetable seeds."
+    puts ""
+    puts "  ------------- Vegetable Seeds -------------"   # user sees a list of vegetable seeds
+    SeedPicker::Seeds.show_all_seeds_by_letter
     choose_a_letter
+  end
+
+  def method_name
+
   end
 
   def choose_a_letter
     puts ""
-    puts "^ - ^ Please choose a group of seeds by its letter from A-Z. Type exit to exit." # user is prompted with a demand
+    puts "^ - ^ Please choose a group of seeds by its letter from A-Z." # user is prompted with a demand
     puts ""
-    puts "  ------------- Vegetable Seeds -------------"   # user sees a list of vegetable seeds
-    SeedPicker::Seeds.show_all_seeds_by_letter ## Seeds class veggie_seed method
-    puts ""
-
-    # user inputs the letter of the parent seed, that letter gives us a list of all the seeds within that letter starting with an index of 1.
     input = gets.strip.to_s.upcase || input = gets.strip.to_i
-
-    # until input == ("A"..."Z")
-    #   puts "Please enter a letter."
-    #   input = gets.strip.to_s.upcase
-    #   break if input == "EXIT"
-    # end
-
+    main = SeedPicker::Seeds.show_all_seeds_by_letter ## Seeds class veggie_seed method
     puts ""
-    puts "     ----- You chose Group: #{input} -----" # user sees a list of all parent seeds with an index of 1.
-    # puts "     1. Amaranth"
-    # puts "     2. Artichoke & Cardoon"
-    # puts "     3. Asparagus"
-
+    puts ""
+    puts " ----- You chose Group: #{input} ----- " # user sees a list of all parent seeds with an index of 1.
+      main[input].collect.with_index(1) do |seed, index|
+        puts "#{index}. #{seed}"
+      end
 
     case input
     when ("A"..."Z")
-      choose_a_parent_seed
+      choose_a_parent_seed(input)
     when "EXIT"
       goodbye
     when (1...10) #fix this
@@ -44,16 +42,22 @@ class SeedPicker::CLI
 
   end
 
-  def choose_a_parent_seed
-    puts ""
-    puts "^ - ^ Please choose a seed by its number. Type back to go back to the list of seeds. Type exit to exit." # user is prompted with a choice
-    puts ""
-    SeedPicker::Seeds.parent_seed
-    puts ""
 
-    # user inputs the number corresponding to the parent seed they want to view
+  def choose_a_parent_seed(input)
+    puts ""
+    puts "^ - ^ Please choose a seed by its number." # user is prompted with a choice
 
-    input = gets.strip.to_i || input = gets.strip.to_s.upcase
+    input = gets.strip.to_i || input = gets.strip.to_s.upcase   # user inputs the number corresponding to the parent seed they want to view
+
+    puts " ----- Group 'letter' - 'number': 'name of seed' #{input} ----- "
+    puts "     Description of seed"
+    SeedPicker::Seeds.get_description
+    puts ""
+    puts " -------- Group 'letter' - 'number': 'name of seed' - Varieties --------" # user sees a list of all parent seeds with an index of 1.
+    # puts "     1. Aurelia's Verde"
+    # puts "     2. Dreadlocks Amaranth"
+    # puts "     3. Elena's Rojo Amaranth"
+    puts ""
 
     case input
     when (1..10)
@@ -70,53 +74,21 @@ class SeedPicker::CLI
 
   def choose_list_of_variety
     puts ""
-    puts "^ - ^ Description"
-    puts "     -------- A - 1: Amaranth --------"
-    puts "     Description of seed"
-    puts ""
-    SeedPicker::Seeds.get_description
-    puts "^ - ^ Please choose a variety by its number. Type back to go back to the list of seeds. Type exit to exit."
-    puts "     -------- A - 1: Amaranth - Varieties --------" # user sees a list of all parent seeds with an index of 1.
-    puts "     1. Aurelia's Verde"
-    puts "     2. Dreadlocks Amaranth"
-    puts "     3. Elena's Rojo Amaranth"
-    puts ""
-    SeedPicker::Seeds.variety_seed
-    # user inputs the number corresponding to the variety they want to view
+    puts "^ - ^ Please choose a variety by its number."
 
     input = gets.strip.to_i || input = gets.strip.to_s.upcase
 
-    case input
-    when (1..10)
-      choose_the_variety
-    when "EXIT"
-      goodbye
-    when "BACK"
-      choose_a_parent_seed
-    else
-      input = gets.strip.to_s.upcase
-    end
-
-  end
-
-  def choose_the_variety
-    puts "     -------- A - 1: Amaranth - Aurelia's Verde --------" # user sees a list of all parent seeds with an index of 1.
+    puts "     -------- Group 'letter' - 'number': 'name of seed' - 'variety_name' --------" # user sees a list of all parent seeds with an index of 1.
     puts "     Description: "
-    SeedPicker::Seeds.variety_seed_description
+
     puts "     Price: "
     puts "     ---------------------------------------------------"
-    SeedPicker::Seeds.variety_seed_info
     puts ""
-    puts "~.~ Type back if you want to see another variety of the same seed. Type main-seed if you want to choose another seed. Type done if you are done."
-    # user inputs the number corresponding to the variety they want to view
-
-    input = gets.strip.to_s.upcase
+    puts "~.~ Type main-seed if you want to choose another seed. Type done if you are done."
 
     case input
     when "MAIN-SEED"
       choose_a_letter
-    when "BACK"
-      choose_list_of_variety
     else "DONE"
       goodbye
     end

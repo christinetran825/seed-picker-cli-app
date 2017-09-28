b940f74aedf1b6dd5ad94e61e93ba51c61e40f95
Christine Tran <christinetran825@gmail.com>
2017-09-28 02:40:59 +0000

    added @variety_description and @variety_url

--- lib/seed_picker/seeds.rb
+++ lib/seed_picker/seeds.rb
@@ -1,6 +1,6 @@
 class SeedPicker::Seeds
 
-  attr_accessor :parent_letter, :parent_seed_name, :parent_seed_url, :veggie_description, :variety_name, :price
+  attr_accessor :parent_letter, :parent_seed_name, :parent_seed_url, :veggie_description, :variety_url, :variety_description, :variety_name, :price
 
   @@all = []
 
@@ -10,6 +10,8 @@ class SeedPicker::Seeds
     @parent_seed_url = parent_seed_url
     @veggie_description = veggie_description
     @variety_name = variety_name
+    @variety_url = variety_url
+    @variety_description = variety_description
     @price = price
     @@all << self #all instances get added into @@all array
   end
@@ -48,21 +50,10 @@ class SeedPicker::Seeds
     end
   end
 
-  # def self.scrape_parent_seeds #scraping the parent seed for the url, url_id, parent_seed_name, and letter
-  #   doc = Nokogiri::HTML(open("http://www.rareseeds.com/store/vegetables/"))
-  #   doc.css(".sitebody .grid_4 h3.itemTitle a").collect do |the_seeds|
-  #     seed = SeedPicker::Seeds.new ##!!!!! CALLING a new instance OBJECT !!!!!!!!
-  #     seed.parent_seed_url = the_seeds["href"]
-  #     seed.parent_seed_name = the_seeds.text
-  #     seed.parent_letter = the_seeds.text[0]
-  #           finder = Nokogiri::HTML(open(seed.parent_seed_url))
-  #           finder.css(".sitebody .grid_9 .mainContent").collect do |the_details|
-  #             seed.veggie_description = finder.css("div#CT_Main_0_pnlHeading .sectionDesc p").text.gsub(/\r\n\t/, "") #description
-  #             seed.variety_name = finder.css(".hawksearch .grid_4 h3.itemTitle a").text #variety name
-  #             seed.price = finder.css(".hawksearch .grid_4 .itemMiniCart .itemPrice").text  #variety price
-  #           end
-  #     seed
-  #   end
-  # end
+  def self.variety_seed_description
+    self.all.collect.with_index(1) do |the_info, index|
+      puts "#{index}. #{the_info.variety_description}"
+    end
+  end
 
 end

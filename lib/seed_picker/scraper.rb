a8e282e78227da6d4305b713c28a91965919229f
Christine Tran <christinetran825@gmail.com>
2017-09-28 03:09:56 +0000

    redefined method

--- lib/seed_picker/scraper.rb
+++ lib/seed_picker/scraper.rb
@@ -4,22 +4,22 @@ binding.pry
   attr_accessor :variety_description
 
   def self.scrape_parent_seeds #scraping the parent seed for the url, url_id, parent_seed_name, and letter
-  doc = Nokogiri::HTML(open("http://www.rareseeds.com/store/vegetables/"))
-  doc.css(".sitebody .grid_4 h3.itemTitle a").collect do |the_seeds|
-    seed = SeedPicker::Seeds.new ##!!!!! CALLING a new instance OBJECT !!!!!!!!
-    seed.parent_seed_url = the_seeds["href"]
-    seed.parent_seed_name = the_seeds.text
-    seed.parent_letter = the_seeds.text[0]
-      finder = Nokogiri::HTML(open(seed.parent_seed_url))
-      finder.css(".sitebody .mainContent div#CT_Main_0_pnlHeading").collect do |the_details|
-        seed.veggie_description = the_details.css("p").text.gsub(/\r\n\t/, "") #description
-      end
-      finder.css(".sitebody .hawksearch .grid_4").collect do |the_info|
-        seed.variety_name = the_info.css("h3.itemTitle a").text #variety name
-        seed.price = the_info.css(".itemMiniCart .itemPrice").text #variety price
-      end
-    seed
+    doc = Nokogiri::HTML(open("http://www.rareseeds.com/store/vegetables/"))
+    doc.css(".sitebody .grid_4 h3.itemTitle a").collect do |the_seeds|
+      seed = SeedPicker::Seeds.new ##!!!!! CALLING a new instance OBJECT !!!!!!!!
+      seed.parent_seed_url = the_seeds["href"]
+      seed.parent_seed_name = the_seeds.text
+      seed.parent_letter = the_seeds.text[0]
+        finder = Nokogiri::HTML(open(seed.parent_seed_url))
+        finder.css(".sitebody .mainContent div#CT_Main_0_pnlHeading").collect do |the_details|
+          seed.veggie_description = the_details.css("p").text.gsub(/\r\n\t/, "") #description
+        end
+        finder.css(".sitebody .hawksearch .grid_4").collect do |the_info|
+          seed.variety_name = the_info.css("h3.itemTitle a").text #variety name
+          seed.price = the_info.css(".itemMiniCart .itemPrice").text #variety price
+        end
+      seed
+    end
   end
-end
 
 end

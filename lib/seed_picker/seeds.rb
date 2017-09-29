66f3f5e8131e86a85a01669195e32af365449e74
Christine Tran <christinetran825@gmail.com>
2017-09-29 09:15:52 +0000

    added save method

--- lib/seed_picker/seeds.rb
+++ lib/seed_picker/seeds.rb
@@ -1,23 +1,26 @@
 class SeedPicker::Seeds
 
-  attr_accessor :parent_letter, :parent_seed_name, :parent_seed_url, :veggie_description, :variety_url, :variety_description, :variety_name, :price
+  attr_accessor :parent_seed_name, :parent_seed_url, :parent_description, :variety_url, :variety_description, :variety_name, :price
 
   @@all = []
 
-  def initialize #new instances have the below methods/properties
-    @parent_letter = parent_letter
-    @parent_seed_name = parent_seed_name
-    @parent_seed_url = parent_seed_url
-    @veggie_description = veggie_description
-    @variety_name = variety_name
-    @variety_url = variety_url
-    @variety_description = variety_description
-    @price = price
-    @@all << self #all instances get added into @@all array
-  end
+  # def initialize #new instances have the below methods/properties
+  #   @parent_seed_name = parent_seed_name
+  #   @parent_seed_url = parent_seed_url
+  #   @parent_description = parent_description
+  #   @variety_name = variety_name
+  #   @variety_url = variety_url
+  #   @variety_description = variety_description
+  #   @price = price
+  #   @@all << self #all instances get added into @@all array
+  # end
 
   def self.all
     @@all
+  end
+
+  def save
+    @@all << self
   end
 
   def self.show_all_seeds_by_letter #listing all vegetable seeds grouped by first letter
@@ -32,21 +35,31 @@ class SeedPicker::Seeds
     end
   end
 
-  def self.parent_seed
-    input = gets.strip.to_s.upcase
-    group = self.show_all_seeds_by_letter
-    list = group[input]
-    list.collect.with_index(1) do |the_info, index|
-      "#{index}. #{the_info}"
-    end
-  end
 
-  def self.get_description
-    parents = self.parent_seed
-    parents.collect.with_index(1) do |the_info, index|
-      "#{index}. #{the_info}"
-      # puts "#{the_info.veggie_description}"
-    end
+  # def self.parent_seed
+  #   input = gets.strip.to_s.upcase
+  #   group = self.show_all_seeds_by_letter
+  #   list = group[input]
+  #   list.collect.with_index(1) do |the_info, index|
+  #     "#{index}. #{the_info}"
+  #   end
+  # end
+
+# binding.pry
+#
+#   s = SeedPicker::Seeds.new
+
+
+  # def self.get_description
+  #   parents = self.parent_seed
+  #   parents.collect.with_index(1) do |the_info, index|
+  #     "#{index}. #{the_info}"
+  #     # puts "#{the_info.parent_description}"
+  #   end
+  # end
+
+  def self.get_parent_description
+    @veggie_description
   end
 
   def self.variety_seed

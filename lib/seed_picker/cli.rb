9fa14e73165b26e48b25de965741f89b13210c60
Christine Tran <christinetran825@gmail.com>
2017-10-03 03:48:13 +0000

    updated all case statements

--- lib/seed_picker/cli.rb
+++ lib/seed_picker/cli.rb
@@ -46,44 +46,40 @@ class SeedPicker::CLI
       puts ""
       puts SeedPicker::Seeds.show_parents(input)
       choose_a_parent_seed
-    when "EXIT"
+    when "DONE"
       goodbye
-    when (1..10) #fix this
-      puts "Please enter a letter."
-      input = gets.strip.to_s.upcase
     else
-      input = gets.strip.to_s.upcase
+      choose_a_letter
     end
 
   end
 
   def choose_a_parent_seed
     puts ""
-    puts "^ - ^ Please choose a seed by its number." # user is prompted with a choice
-    # input = gets.strip.to_i || input = gets.strip.to_s.upcase   # user inputs the number corresponding to the parent seed they want to view
+    puts "^ - ^ Please choose a seed by its number. Enter back to choose another seed." # user is prompted with a choice
     input = gets.strip.to_s.upcase
     num = gets.strip.to_i
 
     case input
     when ("A".."Z") #(1..10)
-      parent_name = SeedPicker::Seeds.others(input, num)
+      parent_name = SeedPicker::Seeds.get_descriptions(input, num)
       puts "testing"
       puts ""
-      puts " ----- Group: #{input} - #{parent_name.upcase} - Description ----- "
+      puts " ----- Group: #{input} - #{parent_name.upcase} ----- "
       # puts SeedPicker::Seeds.testing(input)
       # puts SeedPicker::Seeds.others(input)
-      # puts ""
+      puts ""
       puts " -------- Varieties -------- " # user sees a list of all parent seeds with an index of 1.
       # puts SeedPicker::Seeds.others(input)
       puts ""
       # puts ""
       choose_list_of_variety
-    when "EXIT"
+    when "DONE"
       goodbye
     when "BACK"
       choose_a_letter
     else
-      input = gets.strip.to_s.upcase
+      choose_a_parent_seed
     end
 
   end
@@ -116,17 +112,20 @@ class SeedPicker::CLI
 
     case input
     when "LIST"
-      list_of_seeds
-    else "DONE"
+      self.call
+    when "DONE"
       goodbye
+    else
+      go_back_or_finish
     end
+    
   end
 
   def goodbye
     puts ""
     puts "^ - ^ Goodbye!!!"
     puts ""
-    puts "         ( Thank you for viewing our vegetable seed collection. Seed you again, soon. Have a great day! )"
+    puts "         ( Thank you for viewing our vegetable seed collection. Seed you again, soon!!! )"
     puts "          , "
     puts "  (\\ /)  , "
     puts "  ( . .)  "

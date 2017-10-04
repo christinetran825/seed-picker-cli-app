53de5161a6499df1cdb7f69994b3786d56bd130b
Christine Tran <christinetran825@gmail.com>
2017-10-04 16:29:32 +0000

    updated scrape_variety_seeds(seeds) method to include Varieties class

--- lib/seed_picker/scraper.rb
+++ lib/seed_picker/scraper.rb
@@ -17,15 +17,18 @@ class SeedPicker::Scraper
     doc = Nokogiri::HTML(open("http://www.rareseeds.com" + seed.parent_seed_url))
     doc.css(".sitebody .mainContent").collect do |the_details|
       seed.parent_seed_description = the_details.css(".sectionDesc p").first.text.gsub(/\r\n\t/,"") #parent_description
-      seed.variety_seed_name = the_details.css("h3.itemTitle a").first.text #variety name
+    end
+    doc.css(".sitebody .mainContent .itemWrapper").collect do |the_details|
+      seed = SeedPicker::Varieties.new ##!!!!! CALLING a new instance OBJECT !!!!!!!!
+      seed.variety_seed_name = the_details.css("h3.itemTitle a").text #variety name
       seed.variety_seed_url = the_details.css("h3.itemTitle a").attribute("href").value #variety url
       seed.price = the_details.css(".itemMiniCart .itemPrice").first.text #variety price
     end
   end
 
-  def self.scrape_variety_details(seed) #passing an instance of SeedPicker::Seeds.new which is how we get the seed.parent_seed_url from the previous method
-    doc = Nokogiri::HTML(open("http://www.rareseeds.com" + seed.variety_seed_url))
-    seed.variety_seed_description = doc.css(".sitebody .mainContent .longDescription").text.strip.gsub(/\r\n/, "")
+  def self.scrape_variety_details(variety) #passing an instance of SeedPicker::Seeds.new which is how we get the seed.parent_seed_url from the previous method
+    doc = Nokogiri::HTML(open("http://www.rareseeds.com" + variety.variety_seed_url))
+    variety.variety_seed_description = doc.css(".sitebody .mainContent .longDescription").text.strip.gsub(/\r\n/, "")
   end
 
 end

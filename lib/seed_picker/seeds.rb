d93aca7ff3a6664021bbee75ae44e6f06b5c7721
Christine Tran <christinetran825@gmail.com>
2017-10-04 15:59:34 +0000

    removed methods that created the list that were grouped by first letter. Cleaned up the class.

--- lib/seed_picker/seeds.rb
+++ lib/seed_picker/seeds.rb
@@ -44,51 +44,17 @@ class SeedPicker::Seeds
     all_seeds
   end
 
-  def self.group_by_letter #grouping all parent seeds by their first letters  ###listing all veggie seeds
-    group = self.get_parents
-    group.group_by { |s| s[0,1] } #returns a hash grouped by first letter
-  end
-
   ############### Lists ALL Vegetable Seeds ###############
 
-  def self.listing_all_seeds #listing all vegetable seeds grouped from group_by_letter method hash
-    self.group_by_letter.each do |key, value| #iterates the hash
-      puts "#{key}: #{value}"
-    end
-  end
-
-  ############### Print Parent Seeds ###############
-
-  def self.show_parents(letter)
-    list_parents = self.group_by_letter[letter] #input is being pased to the SeedPicker::Seeds.get_letter method where it's being passed into a hash as a key
-    list_parents.collect.with_index(1) do |the_parent, index| # the results of the SeedPicker::Seeds.testing(input) method is saved as group variable. The group variable is iterated with index of 1.
-      puts "  #{index}. #{the_parent}"
-    end
-  end
-
-  ############### Print Parent Seeds Details ###############
-
-  def self.find_details(letter, num) #gets parent seed chosen from index from show_parents(letter) method
-    group = self.group_by_letter
-    group[letter][num - 1]
-  end
-
-  def self.find(num) #gets parent seed chosen from index from show_parents(letter) method
-    # binding.pry
-    self.all[num-1] #objects are listed starting at 0
-    #how to group each object by it's seed name's first letter?
-  end
-
-  def self.testing
+  def self.listing_all_seeds
     group = self.get_parents
     list_parents = group.collect.with_index(1) do |seed, index| puts "#{index}. #{seed}" end
   end
 
-############### Print Variety Seeds Details ###############
+  ############### Print Parent Seeds Details ###############
 
-  def self.get_varieties(seed) #getting all varieties seeds
-    the_varieties = self.all.collect.with_index(1) { |seeds, index| puts "#{index}. #{seeds.variety_seed_name}" }
-    the_varieties
+  def self.find(num) #gets parent seed chosen from index from show_parents(letter) method
+    self.all[num-1] #objects are listed starting at 0
   end
 
 end

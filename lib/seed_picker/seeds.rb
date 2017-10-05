0e8ad1f3c92a2edb1f821b063b2ae0474715ec78
Christine Tran <christinetran825@gmail.com>
2017-10-05 18:56:34 +0000

    removed find_desc method and unnecessary comments

--- lib/seed_picker/seeds.rb
+++ lib/seed_picker/seeds.rb
@@ -18,8 +18,6 @@ class SeedPicker::Seeds
   ############### Helper Methods ###############
 
   def self.get_parents #getting all parent seeds
-    # all_parents = self.all.collect { |seeds| seeds.parent_seed_name }
-    # all_parents.sort { |a, b| a <=> b }
     all_parents = self.all.collect { |seeds| seeds.parent_seed_name }
     all_seeds = all_parents.sort { |a, b| a <=> b }
     all_seeds.delete("Bulk Vegetables")
@@ -29,7 +27,6 @@ class SeedPicker::Seeds
     all_seeds.delete("Thai Varieties") #try to add this manually?
     all_seeds.delete("The Explorer Series") #try to add this manually?
     all_seeds.delete("William Woys Weaver") #try to add this manually?
-    # all_seeds.delete("Turnips") #try to add this manually?
     all_seeds
   end
 
@@ -46,24 +43,6 @@ class SeedPicker::Seeds
 
   def self.find(num) #gets parent seed
     self.all[num-1] #objects are listed starting at 0
-  end
-
-  ############### Get Parent Seeds Descriptions ###############
-
-  def self.find_desc(seed)
-    case seed.parent_seed_name
-    when "Garlic" || "Ground Cherries" || "Salad Blends" #garlic, ground cherries
-      puts "#{seed.parent_seed_description_b}"
-    when "Gourds" || "Melons" || "Peppers"  || "Squash" || "Tomatoes" #gourds, melons, peppers, squash, tomatoes; no descriptions
-      #there's no description from website
-      puts "#{seed.parent_seed_description_a}"
-      puts ""
-      # if seed.parent_seed_description_c == nil
-      #   puts "Select a variety to view descriptions."
-      # end
-    else
-      puts "#{seed.parent_seed_description_c}" #all seeds
-    end
   end
 
 end

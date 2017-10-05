56207e76ce55eeaeb0d135aa757908ef376c5d51
Christine Tran <christinetran825@gmail.com>
2017-10-05 22:18:29 +0000

    reactivated methods of Gourd seed; updated scrape grouped varieties

--- lib/seed_picker/scraper.rb
+++ lib/seed_picker/scraper.rb
@@ -22,9 +22,9 @@ class SeedPicker::Scraper
       seed.parent_seed_description = doc.css(".sitebody .mainContent .sectionDesc").text.strip
     when "Gourds"
       seed.parent_seed_description = doc.css(".sitebody .mainContent p").children.first.text.gsub(/\r\n\t/,"")
-      # groups = SeedPicker::Grouped_Variety.new
-      # groups.grouped_variety_url = doc.css(".sitebody .mainContent .itemWrapper h3.itemTitle a").attribute('href').value
-      # groups.grouped_variety_name = doc.css(".sitebody .mainContent .itemWrapper h3.itemTitle a").text
+      groups = SeedPicker::Grouped_Variety.new
+      groups.grouped_variety_url = doc.css(".sitebody .mainContent .itemWrapper h3.itemTitle a").attribute('href').value
+      groups.grouped_variety_name = doc.css(".sitebody .mainContent .itemWrapper h3.itemTitle a").first.text
     when "Melon"  #no descriptions
       seed.parent_seed_description = doc.css(".sitebody .mainContent p").first.text
     when "Peppers"  #no descriptions
@@ -38,12 +38,12 @@ class SeedPicker::Scraper
     end
   end
 
-  def self.scrape_grouped_varieties(seed)
-    doc = Nokogiri::HTML(open("http://www.rareseeds.com" + seed.parent_seed_url))
+  def self.scrape_grouped_varieties(groups)
+    doc = Nokogiri::HTML(open(groups.grouped_variety_url))
       groups = SeedPicker::Grouped_Variety.new
       doc.css(".sitebody .mainContent .itemWrapper").collect do |seeds|
-        groups.grouped_variety_url = seeds.css("h3.itemTitle a").attribute('href').value
-        groups.grouped_variety_name = seeds.css("h3.itemTitle a").text
+        groups.grouped_variety_varieties_url = seeds.css("h3.itemTitle a").attribute('href').value
+        groups.grouped_variety_varieties_name = seeds.css("h3.itemTitle a").text
       end
   end
 
